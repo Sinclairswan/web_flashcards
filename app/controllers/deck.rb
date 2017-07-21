07b5b1dc546b4121bcd5ad8b45e091ea43e8467b
Rachel Peace <email": null>
2017-07-21 14:31:03 -0500

    Add seed data to test, add base to deck index erb

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -1,10 +1,10 @@
 get '/decks' do
-  "this will show all available decks to take a round of"
+  @decks = Deck.all
+  erb :'decks/index'
 end
 
 get '/decks/:deck_id' do
-  "this is where you look at a specific deck and decide to take a round
-  will have button to post quiz"
+  erb :'decks/show'
 end
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,28 @@
-user1 = User.create(
-  first_name: "Joe",
-  last_name: "Shmoe",
-  email: "joe.shmoe@example.com",
-  password: "joe"
-  )
+# user1 = User.create(
+#   first_name: "Joe",
+#   last_name: "Shmoe",
+#   email: "joe.shmoe@example.com",
+#   password: "joe"
+#   )
 
-user2 = User.create(
-  first_name: "Melinda",
-  last_name: "Banks",
-  email: "melinda.banks@example.com",
-  password: "melinda"
-  )
+# user2 = User.create(
+#   first_name: "Melinda",
+#   last_name: "Banks",
+#   email: "melinda.banks@example.com",
+#   password: "melinda"
+#   )
 
+cardset = []
+cardset << Card.create(question: "What is Austin?", answer: "city")
+cardset << Card.create(question: "What is Texas?", answer: "state")
+cardset << Card.create(question: "What is the U.S.?", answer: "country")
+cardset << Card.create(question: "What is North America?", answer: "continent")
+
+deck1 = Deck.create(name: "Birds")
+deck2 = Deck.create(name: "Cats")
+deck3 = Deck.create(name: "Dogs")
+deck4 = Deck.create(name: "Coffee")
+deck5 = Deck.create(name: "Computers")
+
+deck1.cards << cardset
 
