9464056baaa2c3444c4d06d293549ce29fa05551
Erika Boren <email": null>
2017-07-21 12:37:44 -0500

    rename password to encrypted_password

--- db/migrate/20170721105906_create_table_users.rb
+++ db/migrate/20170721105906_create_table_users.rb
@@ -3,7 +3,7 @@ class CreateTableUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string   :username
       t.string   :email
-      t.string   :password
+      t.string   :encrypted_password
 
       t.timestamps
     end
