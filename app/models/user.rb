5cacc0951046d2e7d77d4b5a02a06648af128941
Erika Boren <email": null>
2017-07-21 12:36:01 -0500

    add bcrypt to gemfile and env

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ApplicationRecord
   has_many :rounds
-  # Remember to create a migration!
+
+  validates :username, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
+
+  def password
+    @password ||= BCrypt::Password.new(encrypted_password)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.encrypted_password = @password
+  end
+
+  def authenticate(password)
+    self.password == password
+  end
 end
