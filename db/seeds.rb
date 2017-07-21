fa5acba780cc7f4da84f1630a5f4bc3d563e1885
Brittany Restine & Krista Starr <pair+brittrestine.kristastarr@devbootcamp.com>
2017-07-21 14:57:40 -0500

    working database with deck2

---
+++ db/seeds.rb
@@ -0,0 +1,56 @@
+content_for_deck_2 = {
+  title: "All About Raccoons"
+}
+
+raccoons = Deck.create(content_for_deck_2)
+
+
+
+
+card_1_for_deck_2 = {
+  deck_id: 2,
+  question: "What is a male raccoon called?",
+  answer: "Boar"
+}
+
+card_1 = Card.create(card_1_for_deck_2)
+
+card_2_for_deck_2 = {
+  deck_id: 2,
+  question: "What is a female raccoon called?",
+  answer: "Sow"
+}
+
+card_2 = Card.create(card_2_for_deck_2)
+
+card_3_for_deck_2 = {
+  deck_id: 2,
+  question: "Are raccoons herbivorous, carnivorous, omnivorous?",
+  answer: "omnivorous"
+}
+
+card_3 = Card.create(card_3_for_deck_2)
+
+card_4_for_deck_2 = {
+  deck_id: 2,
+  question: "True or False? Are Raccoons nocturnal?",
+  answer: "True"
+}
+
+card_4 = Card.create(card_4_for_deck_2)
+
+card_5_for_deck_2 = {
+  deck_id: 2,
+  question: "True or False? Do Raccoons hibernate?",
+  answer: "False"
+}
+
+card_5 = Card.create(card_5_for_deck_2)
+
+card_6_for_deck_2 = {
+  deck_id: 2,
+  question: "True or False? Raccoons can run 15 miles per hours?",
+  answer: "True"
+}
+
+card_6 = Card.create(card_6_for_deck_2)
