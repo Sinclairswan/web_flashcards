d6fb80339899d7ed6f654d24e87548c82dc964e8
Brittany Restine & Katy McCloskey & Krista Starr & Regina DeAngelis <pair+brittrestine.katymccloskey.kristastarr.reginad1@devbootcamp.com>
2017-07-21 14:55:32 -0500

    fix typos to seed database correctly

--- db/migrate/20170721122456_create_decks.rb
+++ db/migrate/20170721122456_create_decks.rb
@@ -2,11 +2,11 @@ class CreateDecks < ActiveRecord::Migration[5.0]
 
 	def change
 
-		create table :deck do |t|
+		create_table :decks do |t|
 
-			t. string :deck_title, null :false 
+			t.string :title, null: false
 
-			t.timestamps 
+			t.timestamps
 		end
 	end
-end 
+end
