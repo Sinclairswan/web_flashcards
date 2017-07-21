013eb0008a6bcf775b36565a99282c68b4a7da51
Brittany Restine & Krista Starr <pair+brittrestine.kristastarr@devbootcamp.com>
2017-07-21 16:09:50 -0500

    register, login, logout, css, bcrypt

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -22,19 +22,19 @@ get '/login' do
   erb :'/login'
 end
 
-# post '/login' do
-#    user = User.find_by(email: params[:email])
-#   if user && user.authenticate(params[:password])
-#     session[:user_id] = user.id
-#     redirect '/'
-#   else
-#     @error = "Invalid Password, Try Again."
-#     erb :'/entries/login'
-#   end
-# end
+post '/login' do
+   user = User.find_by(email: params[:email])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    @error = "Invalid Password, Try Again."
+    erb :'/entries/login'
+  end
+end
 
 
-# get '/logout' do
-#   session.delete(:user_id)
-#   redirect '/'
-# end
+get '/logout' do
+  session.delete(:user_id)
+  redirect '/'
+end
