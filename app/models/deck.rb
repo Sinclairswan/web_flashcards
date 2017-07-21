7ceeef225164636cd8bddeeb26841a347976ceb9
Sinclair Swan <email": null>
2017-07-21 18:21:39 -0500

    removed guesses table

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,5 +1,4 @@
 class Deck < ApplicationRecord
   has_many :cards
-  has_many :guesses
   # Remember to create a migration!
 end
--- db/migrate/20170721105859_create_table_rounds.rb
+++ db/migrate/20170721105859_create_table_rounds.rb
@@ -3,6 +3,8 @@ class CreateTableRounds < ActiveRecord::Migration[5.0]
     create_table :rounds do |t|
       t.integer :user_id
       t.integer :deck_id
+      t.integer :result
+      t.integer :total_result
 
       t.timestamps
     end
