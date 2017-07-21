d0bbaa2434e8db5c40d0fae406a415be9a2dd047
Brittany Restine & Katy McCloskey & Krista Starr & Regina DeAngelis <pair+brittrestine.katymccloskey.kristastarr.reginad1@devbootcamp.com>
2017-07-21 12:43:33 -0500

    create table for guesses and rounds, and associations in models

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,3 +1,5 @@
 class Guess < ApplicationRecord
+  belongs_to :round
+  belongs_to :user, through: :round
   # Remember to create a migration!
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,3 +1,6 @@
 class Round < ApplicationRecord
+  has_many :guesses
+  belongs_to :deck
+  belongs_to :user
   # Remember to create a migration!
 end
