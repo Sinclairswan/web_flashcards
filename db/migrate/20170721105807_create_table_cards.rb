7b3540126575808078d410e5c17998e748dbb1c1
Dayne Beck <email": null>
2017-07-21 13:41:08 -0500

    seeded cards and decks

--- db/migrate/20170721105807_create_table_cards.rb
+++ db/migrate/20170721105807_create_table_cards.rb
@@ -1,7 +1,7 @@
 class CreateTableCards < ActiveRecord::Migration[5.0]
   def change
     create_table :cards do |t|
-     t.string    :questions
+     t.string    :question
      t.string    :answer
      t.integer   :deck_id
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
+require 'csv'
+
+CSV.foreach("/Users/apprentice/web-flashcards-challenge/sample_decks/jeopardy_csv.csv", headers: true, header_converters: :symbol) do |row|
+
+  deck = Deck.find_or_create_by(name: row.to_hash[:name])
+  deck_id = deck.id
+  card = {question: row.to_hash[:question],
+          answer: row.to_hash[:answer],
+          deck_id: deck_id }
+
+  Card.create(card)
+
+end
 
 
 
