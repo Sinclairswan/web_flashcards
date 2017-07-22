d61c87cb69906e4f445b46725c66531ec61716b4
Katie McNeil <katie.mcneil@live.com>
2017-07-21 19:26:24 -0500

    Finish views for questions/round summary/profile summary, etc.

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -2,4 +2,6 @@ class Guess < ApplicationRecord
   belongs_to :round
   belongs_to :card
   # Remember to create a migration!
+
 end
+
--- app/models/round.rb
+++ app/models/round.rb
@@ -3,4 +3,23 @@ class Round < ApplicationRecord
   belongs_to :user
   has_many :guesses
   # Remember to create a migration!
+
+  def first_try_guesses
+    guesses = 0
+    self.guesses.each do |guess|
+      if guess.count == 1
+        guesses += 1
+      end
+    end
+    guesses
+  end
+
+  def total_guesses
+    guesses = 0
+    self.guesses.each do |guess|
+      guesses += guess.count
+    end
+    guesses
+  end
+
 end
