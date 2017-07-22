0a3d528fbd5390c0466a33d9a62698b3de19f34f
Sinclair Swan <email": null>
2017-07-22 16:14:23 -0500

    link decks to card view

--- app/controllers/card.rb
+++ app/controllers/card.rb
@@ -1,12 +1,19 @@
-
-#card_display
-get '/card_display' do
-  erb :'cards/card_display'
+get '/card_display/deck/:id' do
+ @deck =Deck.find(params[:id])
+ @cards = @deck.cards
+  erb :"cards/card_display"
 end
 
-get '/deck/:id' do
-  @deck = Deck.find(params[:id])
-  @cards = @deck.cards
-  erb :'/decks/show'
-
+post '/card_display/deck/:id' do
+ @deck =Deck.find(params[:id])
+ @cards = @deck.cards
+ card = @cards.find_by_deck_id(@deck.id)
+ card_id = card.id
+ answer = params[:card][:answer]
+ if answer ==!nil && card_id ==!nil
+ @response = right_or_wrong(answer, card_id)
+else
+  @response = "please enter something"
+ end
+erb :"cards/card_display"
 end
--- app/helpers/game.rb
+++ app/helpers/game.rb
@@ -1,4 +1,19 @@
 helpers do
+
+def right_or_wrong(answer, card_id)
+card = Card.find(card_id)
+    if answer.downcase.split(//)  == card.answer.downcase.split(//)
+    "Correct!"
+   else
+    "Not Right"
+   end
+end
+
+
+
+
+
+
 def cut_the_deck(answer, card_id)
 	#compare the answer to the user input and move from the card
 	@correct = []
