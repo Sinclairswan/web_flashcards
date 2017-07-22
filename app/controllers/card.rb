808b2e97fcc62ef63c8312eaf8342a7653797cf7
Dayne Beck <email": null>
2017-07-21 21:37:10 -0500

    1 card question at a time displaying, we may need to refactor to account for guess objects...tired af

--- app/controllers/card.rb
+++ app/controllers/card.rb
@@ -1,12 +1,13 @@
 
 #card_display
-get '/card_display' do
-  erb :'cards/card_display'
-end
+# get '/card_display' do
+#   erb :'cards/card_display'
+# end
 
 get '/deck/:id' do
   @deck = Deck.find(params[:id])
-  @cards = @deck.cards
-  erb :'/decks/show'
+  @ordered_cards = @deck.cards.order(:id)
 
+
+  erb :'/decks/show'
 end
