8fbdf480cfcec9a3e70291b04fff88771b48b63b
Sinclair Swan <email": null>
2017-07-22 19:00:06 -0500

    :blocked af

--- app/controllers/card.rb
+++ app/controllers/card.rb
@@ -1,19 +1,19 @@
 get '/card_display/deck/:id' do
  @deck =Deck.find(params[:id])
  @cards = @deck.cards
+ # binding.pry
   erb :"cards/card_display"
 end
 
 post '/card_display/deck/:id' do
  @deck =Deck.find(params[:id])
  @cards = @deck.cards
- card = @cards.find_by_deck_id(@deck.id)
- card_id = card.id
+ @card = @cards.find_by_deck_id(@deck.id)
+ card_id = @card.id
  answer = params[:card][:answer]
- if answer ==!nil && card_id ==!nil
+
  @response = right_or_wrong(answer, card_id)
-else
-  @response = "please enter something"
- end
+# binding.pry
+
 erb :"cards/card_display"
 end
--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -2,3 +2,30 @@ get '/' do
   @decks = Deck.all
   erb :'/home/home'
 end
+
+get '/deck/:id' do
+   @deck = Deck.find(params[:id])
+   @cards_in_order = @deck.cards.order(:id)
+   @cards = @deck.cards
+
+  #erb :"/cards/card_display"
+  redirect "/deck/#{@deck.id}/card/#{@cards.first.id}"
+end
+
+ get '/deck/:id/card/:id' do
+ @deck = Deck.find(params[:id])
+ @round = Round.new(deck_id: @deck.id)
+ @deck.cards.select do |deck|
+  deck.map do |card|
+  @card = card
+  @question= card.question
+end
+  end
+end
+ # @card.id + 1 until end of deck
+ #  @deck = Deck.find(params[:id])
+ #
+
+
+
+
